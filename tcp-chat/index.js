ff74c205d4e4e16ab1b37d2d4660149e0e33f2b1
Terry Tong <blessedoverlap@gmail.com>
2013-05-17 07:50:25 +0800

    Finished the tcp-chat, before wrap up

--- tcp-chat/index.js
+++ tcp-chat/index.js
@@ -18,8 +18,34 @@ var server = net.createServer(function (conn) {
   var nickname;
 
   conn.on('data', function(data){
+    // Remove the "enter" character
+    data = data.replace('\r\n', '');
+
+    // the first piece of data you expect is the nickname
+    if (!nickname) {
+      if (users[data]) {
+        conn.write('\033[93m> nickname already in use. try again:\033[39m ');
+        return;
+      } else {
+        nickname = data;
+        users[nickname] = conn;
+
+        for (var i in users) {
+          users[i].write('\033[90m > ' + nickname + ' joined the room\033[39m\n');
+        }
+      }
+    } else {
+      // otherwise you consider it a chat message
+      for (var i in users) {
+        if (i != nickname) {
+          users[i].write('\033[96m > ' + nickname + ':\033[39m ' + data + '\n');
+        }
+      }
+    }
+
     console.log(data);
   });
+
   conn.on('close', function () {
     count--;
   });
